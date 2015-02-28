1fd1d12c2b95b691fe7eab0fd07f33af0ffdf5ad
stevecass <steven@mumsnet.com>
2015-02-28 15:09:15 -0500

    Create a product model

--- server.js
+++ server.js
@@ -10,34 +10,22 @@ app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/mydb');
 
-var productSchema = mongoose.Schema({ name: String, price: Number });
-productSchema.prettyPrice = function() {
-   return "I cost $" + this.price;
+var Product = require('./models/product.js')(mongoose);
+
+var setObjectFieldsFromParams = function(obj, params) {
+  for (k in obj.schema.paths) {
+    if (params[k]) {
+      obj[k] = params[k];
+    }
+  }
 };
 
-var Product = mongoose.model('Product', productSchema);
-
-//Lets set up some sample data
-/*
-var sampleProducts = [   new Product({name: 'Book', price:14.99 }),   
-new Product({name: 'DVD', price:4.76 }),   
-new Product({name: 'Washing Machine', price:714.99 })  ];
-
-
-sampleProducts.forEach(function(ele) { 
-  ele.save(function (err, ele) {
-    if (err) {
-      console.erroror(err);
-    }
-  });
-});
-*/
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.all('/api/products/*', function(req, res, next){
+app.all('/api/products/:id', function(req, res, next){
   // If we get a product id it should be valid - else return a 404
   if (req.params.id) {
     if (!objectID.isValid(req.params.id)) {
@@ -77,10 +65,8 @@ app.get('/api/products/:id', function(req, res) {
 });
 
 app.post('/api/products', function(req, res){
-  product = new Product({
-    name: req.body.name,
-    price: req.body.price
-  });
+  product = new Product();
+  setObjectFieldsFromParams(product, req.body);
   product.save(function(err, newprod) {
     if (err) {
       res.status(500).send('Error');
@@ -93,7 +79,6 @@ app.post('/api/products', function(req, res){
 });
 
 app.put('/api/products/:id', function(req, res){
-  cb = function(err, product) {};
   product = Product.findById(req.params.id, function(err, product){
     if (err) {
       console.error(err);
@@ -101,8 +86,7 @@ app.put('/api/products/:id', function(req, res){
     } else if (product === null) {
       res.status(404).send('Not found');
     } else {
-      product.name = req.body.name;
-      product.price = req.body.price;
+      setObjectFieldsFromParams(product, req.body);
       product.save(function(err, product){
         if (err) {
           console.error(err);
