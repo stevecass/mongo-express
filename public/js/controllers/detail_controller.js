a09d5f2d98e8b5110905b3f0db35bc7ff8f7334b
Steven Cassidy <steven@mumsnet.com>
2015-03-01 21:58:15 -0500

    Improve detail listing

--- public/js/controllers/detail_controller.js
+++ public/js/controllers/detail_controller.js
@@ -1,8 +1,7 @@
 angular.module('products'). controller('DetailController', ['$scope', '$location', '$route', 'Product', function($scope, $location, $route, Product ){
   console.log('DetailController init');
-  $scope.data = {};
   Product.getOne({id: $route.current.params.id}).$promise.then(function(data){
-   $scope.data.product = data; 
+   $scope.product = data; 
   });
 }]);
 
