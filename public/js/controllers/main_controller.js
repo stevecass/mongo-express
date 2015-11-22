8eee5438ba557931f07a704e795b26b98f5587af
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-22 16:04:13 -0500

    Remove some logging; fix whitespace

--- public/js/controllers/main_controller.js
+++ public/js/controllers/main_controller.js
@@ -1,8 +1,8 @@
-angular.module('products'). controller('MainController', 
+angular.module('products'). controller('MainController',
   ['$scope', '$location', 'Product', 'Shared',
   function($scope, $location, Product, Shared){
-  console.log('MainController init');
   $scope.data = {};
+
 
   Product.query().$promise.then(function(data){
     $scope.data.products = data;
@@ -13,9 +13,8 @@ angular.module('products'). controller('MainController',
   $scope.deleteOne = function(id) {
     Product.remove({id: id}).$promise.then(function(data){
      $scope.data.products = $scope.data.products.filter(function(ele){
-      console.log(ele._id + " " + id);
       return ele._id !== id;
-     }); 
+     });
     });
   };
 
