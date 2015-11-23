5e4fb8e167852784f88e476b0dea1044e5dceb1c
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-22 21:40:59 -0500

    Add a client-side logout function

--- public/js/controllers/login_controller.js
+++ public/js/controllers/login_controller.js
@@ -1,13 +1,18 @@
 angular.module('products')
-  .controller('LoginController', ['$scope', '$http', 'Shared', 'User',
-  function($scope, $http, Shared, User){
+  .controller('LoginController', ['$scope', '$http', '$location', 'Shared', 'User',
+  function($scope, $http, $location, Shared, User){
     $scope.currentUser = Shared.currentUser;
+
+    if($location.path() === '/logout') {
+      Shared.currentUser = null;
+    }
 
     $scope.login = function() {
       params = {username: $scope.username, password: $scope.password};
       User.login(params).$promise
       .then(function(data){
         Shared.currentUser = data;
+        $location.path('/main');
       })
       .catch(function(error){
         console.log('catch', error);
