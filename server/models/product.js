375465f3d1b7c450a119a6113b75f453e046bbe6
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-22 22:33:05 -0500

    Add comment to product schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,8 +1,10 @@
 module.exports = function(mgoose) {
-  var productSchema = mgoose.Schema({ 
-    name: String, 
+  var productSchema = mgoose.Schema({
+    name: String,
     description: String,
-    price: Number });
+    price: Number,
+    comments: [{ body: String, date: Date }]
+   });
 
   return mgoose.model('Product', productSchema);
 };
