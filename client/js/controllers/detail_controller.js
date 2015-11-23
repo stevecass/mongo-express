d9430b333f47a99f44f1cb430b7ab9970e5a5358
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-22 23:12:45 -0500

    Add the client-side addComment function

--- client/js/controllers/detail_controller.js
+++ client/js/controllers/detail_controller.js
@@ -5,5 +5,17 @@ angular.module('products'). controller('DetailController',
    $scope.product = data;
    Shared.product = data;
   });
+
+    $scope.addComment = function() {
+      var newComment = {body: $scope.commentBody, date: new Date()};
+      $scope.product.comments.push(newComment);
+
+      Product.update($scope.product).$promise
+      .then(function(response) {
+        $scope.product = response;
+        Shared.product = response;
+        $scope.commentBody = "";
+      });
+    };
 }]);
 
